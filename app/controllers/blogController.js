5cba05f5b879584a5ec7315f39bbd6cc8fcbddb7
Kavya <kavyamaniyan13@gmail.com>
2019-07-22 23:51:18 +0530

    completed blog CRUD operations

--- app/controllers/blogController.js
+++ app/controllers/blogController.js
@@ -29,9 +29,25 @@ router.get('/:id', authenticateUser, (req,res)=>{
         .catch(err=>res.send(err))
 })
 
-router.put('/' , authenticateUser, (req,res)=>{
+router.put('/:id' , authenticateUser, (req,res)=>{
+    const id = req.params.id
     const body = req.body
-    blog.findOneAndUpdate()
+    Blog.findOneAndUpdate({
+        user: req.user._id,
+        _id: id
+    }, {$set: body}, {new: true})
+        .then(blog=>res.send(blog))
+        .catch(err=>res.send(err))
+})
+
+router.delete('/:id', authenticateUser, (req,res)=>{
+    const id = req.params.id
+    Blog.findOneAndDelete({
+        user: req.user._id,
+        _id: id
+    })  
+        .then(()=>res.send("successfully deleted"))
+        .catch((err)=>res.send(err))
 })
 
 module.exports = router
