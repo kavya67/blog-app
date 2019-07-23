5c1204becf70e8662a8e63d6b939f6145bd9311b
Kavya <kavyamaniyan13@gmail.com>
2019-07-23 14:05:29 +0530

    initial reac/redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,30 @@
-import {createStore, combineReducers} from 'redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from './config/config'
+import {Provider} from 'react-redux'
 
-const store = ()=>{
-    
+import App from './App'
+import configureStore from './store/ConfigureStore'
+import {setUser} from './action/userAction'
+
+const store = configureStore()
+store.subscribe(()=>{
+    console.log('redux store app' , store.getSate())
+})
+
+if(localStorage.getItem('userAuthToken')){
+    axios.get('/users/account', {
+        headers: {
+            'x-auth': localStorage.getItem('userAuthToken')
+        }
+    })
+        .then(response=>{
+            store.dispatch(setUser(response.data))
+        })
 }
+
+const jsx = <Provider store={store}>
+    <App/>
+</Provider>
+
+ReactDOM.render(jsx,document.getElementById('root'))
